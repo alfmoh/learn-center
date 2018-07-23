2347f2bdb3cdacd7bd6e9e88e5a04ba1d560c953
alf <alfmohenu@outlook.com>
2018-07-23 20:16:47 +0300

    testimonials converted to carousels.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,15 @@
 import  MobileMenu  from "./modules/mobile-menu";
+import $ from "jquery";
+import * as slick from "slick-carousel";
 
 new MobileMenu();
+
+$(document).ready(function(){
+    $('.testimonials-list').slick({
+        dots: true,
+        infinite: true,
+        arrows: true,
+        autoplay: true,
+        arrows:false
+    });
+  });
